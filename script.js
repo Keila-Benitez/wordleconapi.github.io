90e63430f249a1a2a4b9d192672cfa1cd0fd5e2d
Keila-Benitez <144249084+Keila-Benitez@users.noreply.github.com>
2024-04-21 18:39:17 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,74 @@
+let diccionario = ["ALLOW", "ACTOR", "WINGS", "WRITE", "HOUSE"]
+let indice =  Math.floor(Math.random() * diccionario.length);
+console.log (indice);
+let palabra;
+//Definicion de la API
+fetch("https://random-word-api.herokuapp.com/word?lang=es&length=5")
+ 	.then(response => response.json())
+ 	.then(response => {
+         console.log(response)
+         palabra = response[0].toUpperCase()
+     })
+ 	.catch(err => console.error(err));
+console.log (palabra);
+
+const button = document.getElementById ("guess-button");
+button.addEventListener("click", intentar);
+const input = document.getElementById("guess-input");
+let intentos = 6;
+
+button.addEventListener("click", intentar);
+
+function intentar() {
+    const INTENTO = leerIntento();
+    const GRID = document.getElementById("grid");
+    const ROW = document.createElement("div");
+    ROW.className = "row";
+    if (INTENTO.length != 5) {
+        alert("La palabra contine 5 letras");
+        return;
+    }
+    if (INTENTO === palabra) {
+        const emoji = "😃";
+        terminar("<h1>GANASTE😃</h1>");
+        button.disabled = true;
+        return;
+    }
+    for (let i in palabra) {
+        const SPAN = document.createElement("span");
+        SPAN.className = "letter";
+        if (INTENTO[i] === palabra[i]) {
+            SPAN.innerHTML = INTENTO[i];
+            SPAN.style.backgroundColor = "green";
+        } else if (palabra.includes(INTENTO[i])) {
+            SPAN.innerHTML = INTENTO[i];
+            SPAN.style.backgroundColor = "yellow";
+            console.log(INTENTO[i], "AMARILLO");
+        } else {
+            SPAN.innerHTML = INTENTO[i];
+            SPAN.style.backgroundColor = "gray";
+            console.log(INTENTO[i], "GRIS");
+        }
+        ROW.appendChild(SPAN);
+    }
+    GRID.appendChild(ROW);
+    intentos--;
+    if (intentos === 0) {
+        const emoji = "😖";  
+        terminar("<h1>PERDISTE😖</h1>");
+        button.disabled = true;
+    }
+}
+    function leerIntento(){
+        let intento = document.getElementById("guess-input");
+        intento = intento.value;
+        intento = intento.toUpperCase(); 
+        return intento;
+    }
+    function terminar(mensaje){
+        const input = document.getElementById("guess-input");
+        input.disabled = true;
+        buttondisabled = true;
+        let contenedor = document.getElementById('mensaje');
+        contenedor.innerHTML = mensaje;
+    }
